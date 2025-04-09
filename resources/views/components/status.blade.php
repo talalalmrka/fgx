ed93234b91ea0c1f722f7b69f4da19517ab1703c
talalalmrka <talalminfo@gmail.com>
2025-04-09 22:02:55 +0300

    Bump version to 1.1.7 and enhance status component with improved message handling and alert rendering

--- resources/views/components/status.blade.php
+++ resources/views/components/status.blade.php
@@ -1,61 +1,97 @@
 @props([
     'id' => 'status',
     'class' => '',
+    'soft' => false,
+    'outline' => false,
+    'size' => null,
+    'atts' => [],
 ])
-
-@if (session()->has($id))
-    @if (is_array(session($id)))
-        @foreach (session($id) as $msg)
-            <div {!! $attributes->merge([
-                'class' => css_classes(['alert flex-space-2', 'alert-success', $class => $class]),
-            ]) !!}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-check-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                    <path
-                        d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
-                </svg>
-                <div>{{ $msg }}</div>
-            </div>
+@php
+    $soft = $soft ?? $attributes->has('soft');
+    $outline = $outline ?? $attributes->has('outline');
+    $atts = array_merge($attributes->getAttributes(), $atts);
+    $successMessage = session()->has($id) ? session($id) : null;
+@endphp
+@if ($successMessage)
+    @if (is_array($successMessage))
+        @foreach ($successMessage as $msg)
+            @component('fgx::components.alert', [
+                'type' => 'success',
+                'class' => $class,
+                'soft' => $soft,
+                'outline' => $outline,
+                'size' => $size,
+                'atts' => $atts,
+                'content' => $msg,
+            ])
+            @endcomponent
         @endforeach
-    @elseif (is_string(session($id)))
-        <div {!! $attributes->merge([
-            'class' => css_classes(['alert flex-space-2', 'alert-success', $class => $class]),
-        ]) !!}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-check-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                <path
-                    d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
-            </svg>
-            <div>{{ session($id) }}</div>
-        </div>
+    @elseif (is_string($successMessage))
+        @component('fgx::components.alert', [
+            'type' => 'success',
+            'class' => $class,
+            'soft' => $soft,
+            'outline' => $outline,
+            'size' => $size,
+            'atts' => $atts,
+            'content' => $successMessage,
+        ])
+        @endcomponent
     @endif
 @endif
 
 @error($id)
-    <div {!! $attributes->merge([
-        'class' => css_classes(['alert flex-space-2', 'alert-error', $class => $class]),
-    ]) !!}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle"
-            viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-            <path
-                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-        </svg>
-        <div>{{ $message }}</div>
-    </div>
+    @if (is_array($message))
+        @foreach ($message as $emsg)
+            @component('fgx::components.alert', [
+                'type' => 'error',
+                'class' => $class,
+                'soft' => $soft,
+                'outline' => $outline,
+                'size' => $size,
+                'atts' => $atts,
+                'content' => $emsg,
+            ])
+            @endcomponent
+        @endforeach
+    @elseif(is_string($message))
+        @component('fgx::components.alert', [
+            'type' => 'error',
+            'class' => $class,
+            'soft' => $soft,
+            'outline' => $outline,
+            'size' => $size,
+            'atts' => $atts,
+            'content' => $message,
+        ])
+        @endcomponent
+    @endif
 @enderror
+
 @error("$id.*")
-    <div {!! $attributes->merge([
-        'class' => css_classes(['alert flex-space-2', 'alert-error', $class => $class]),
-    ]) !!}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle"
-            viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-            <path
-                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-        </svg>
-        <div>{{ $message }}</div>
-    </div>
+    @if (is_array($message))
+        @foreach ($message as $emsg)
+            @component('fgx::components.alert', [
+                'type' => 'error',
+                'class' => $class,
+                'soft' => $soft,
+                'outline' => $outline,
+                'size' => $size,
+                'atts' => $atts,
+                'content' => $emsg,
+            ])
+            @endcomponent
+        @endforeach
+    @elseif(is_string($message))
+        @component('fgx::components.alert', [
+            'type' => 'error',
+            'class' => $class,
+            'soft' => $soft,
+            'outline' => $outline,
+            'size' => $size,
+            'atts' => $atts,
+            'content' => $message,
+        ])
+        @endcomponent
+    @endif
 @enderror
