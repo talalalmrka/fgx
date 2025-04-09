5e3f3d92fb38db9a134b80211f1fe950c665aea2
talalalmrka <talalminfo@gmail.com>
2025-04-09 22:25:41 +0300

    Fix collapse-card ID generation and add collapse component with toggle functionality

--- resources/views/components/collapse.blade.php
+++ resources/views/components/collapse.blade.php
@@ -1,5 +1,5 @@
 @props([
-    'id' => uniqid('collsapse-card-'),
+    'id' => uniqid('collsapse-'),
     'title' => null,
     'icon' => null,
 ])
@@ -15,17 +15,16 @@
         this.open = localStorage.getItem(this.collapseId) === 'true';
     }
 }" class="card" x-cloak>
-    <div class="card-header flex-space-2" :class="{ 'border-b-0': !open }">
-        <div class="card-title grow text-primary flex-space-2 py-1">
-            @if ($icon)
-                <i class="icon {{ $icon }}"></i>
-            @endif
-            <span>{{ $title ?? '' }}</span>
+    @if (isset($header))
+        <div class="card-header flex-space-2" :class="{ 'border-b-0': !open }">
+            <div class="card-title grow text-primary flex-space-2 py-1">
+                {!! $header !!}
+            </div>
+            <button type="button" class="flex items-center" x-on:click.prevent="toggle">
+                <fgx:collapse-icon />
+            </button>
         </div>
-        <button type="button" class="flex items-center" x-on:click.prevent="toggle">
-            <fgx:collapse-icon />
-        </button>
-    </div>
+    @endif
     <div class="card-body" x-show="open">
         {{ $slot }}
     </div>
